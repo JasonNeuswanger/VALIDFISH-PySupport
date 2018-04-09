3f83d5734e5458a80403ab6ed7176e09b20944df
Jason Neuswanger <jason@troutnut.com>
2018-04-09 02:41:46 -0700

    Tweaking Bayesian optimization testing

--- bayes_opt_test_batch_builder.py
+++ bayes_opt_test_batch_builder.py
@@ -13,33 +13,38 @@ methods = ('ucb', 'ei', 'mixed')
 
 scalings = ('linear', 'log')
 
+start = True
+
 for fish_label in fish_labels:
     for method in methods:
         for scaling in scalings:
-            batch_name = re.sub(r'\W+', '', fish_label) + '_' + method + '_' + scaling  # strip out non-alphanumeric characters from fish label for job name
-            batch_file_contents = """
-            #PBS -S /bin/bash
-            #PBS -N {2}
-            #PBS -q batch
-            #PBS -l nodes=1:ppn=28:Intel
-            #PBS -l walltime=24:00:00
-            #PBS -l mem=60gb
-            #PBS -M jasonneuswanger@gmail.com
-            #PBS -m ae
-            
-            cd $PBS_O_WORKDIR
-            
-            echo
-            echo "Job ID: $PBS_JOBID"
-            echo "Queue:  $PBS_QUEUE"
-            echo "Cores:  $PBS_NP"
-            echo "Nodes:  $(cat $PBS_NODEFILE | sort -u | tr '\n' ' ')"
-            echo
-            
-            python bayes_opt_test_batch.py '{0}' '{1}' '{2}' '{3}'
-            """.format(method, scaling, batch_name, fish_label)
+            if start:
+                batch_name = re.sub(r'\W+', '', fish_label) + '_' + method + '_' + scaling  # strip out non-alphanumeric characters from fish label for job name
+                batch_file_contents = """
+                #PBS -S /bin/bash
+                #PBS -N {2}
+                #PBS -q batch
+                #PBS -l nodes=1:ppn=28:Intel
+                #PBS -l walltime=24:00:00
+                #PBS -l mem=60gb
+                #PBS -M jasonneuswanger@gmail.com
+                #PBS -m ae
+                
+                cd $PBS_O_WORKDIR
+                
+                echo
+                echo "Job ID: $PBS_JOBID"
+                echo "Queue:  $PBS_QUEUE"
+                echo "Cores:  $PBS_NP"
+                echo "Nodes:  $(cat $PBS_NODEFILE | sort -u | tr '\n' ' ')"
+                echo
+                
+                python bayes_opt_test_batch.py '{0}' '{1}' '{2}' '{3}'
+                """.format(method, scaling, batch_name, fish_label)
 
-            with open('{0}.sh'.format('batches/' + batch_name), 'w') as batch_file:
-                batch_file.write(batch_file_contents)
+                with open('{0}.sh'.format('batches/' + batch_name), 'w') as batch_file:
+                    batch_file.write(batch_file_contents)
 
-            # os.system("qsub 'batches/{0}.sh'".format(batch_name))
+                os.system("qsub 'batches/{0}.sh'".format(batch_name))
+
+                start = False
