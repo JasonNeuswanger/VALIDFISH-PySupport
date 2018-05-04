401f03ee5d039f35daab53037877e7af0bc888ec
Jason Neuswanger <jason@troutnut.com>
2018-05-03 17:29:23 -0700

    Misc.

--- calibration_batch_builder.py
+++ calibration_batch_builder.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 # CONFIGURATION FOR A GIVEN JOB
 
@@ -10,14 +11,15 @@ FISH_GROUP = "calibration_five_of_each"
 # COMMON CODE FOR ANY JOB
 
 for i in range(N_NODES):
-    batch_name = "{0} Node {1}".format(JOB_NAME, i)
+    batch_name_raw = "{0} Node {1}".format(JOB_NAME, i)
+    batch_name = re.sub(r'\W+', '', batch_name_raw)
     batch_file_contents = """
     #PBS -S /bin/bash
     #PBS -N {job_name}
     #PBS -q batch
-    #PBS -l nodes={nodes}:ppn=28:Intel
+    #PBS -l nodes=1:ppn={cores}:Intel
     #PBS -l walltime=96:00:00
-    #PBS -l mem=30gb
+    #PBS -l mem=10gb
     #PBS -M jasonneuswanger@gmail.com
     #PBS -m ae
     
@@ -31,7 +33,7 @@ for i in range(N_NODES):
     echo
     
     python calibration_job_runner.py '{job_name}' '{fish_group}'
-    """.format(nodes=CORES_PER_NODE, job_name=JOB_NAME.replace(" ", ""), fish_group=FISH_GROUP)
+    """.format(cores=CORES_PER_NODE, job_name=JOB_NAME, fish_group=FISH_GROUP)
 
     with open('{0}.sh'.format('batches/' + batch_name), 'w') as batch_file:
         batch_file.write(batch_file_contents)
